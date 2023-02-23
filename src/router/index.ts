4c57e4aeb631cffdf33409838550a1e83e74a671
rjvandel <r.j.vanandel@students.uu.nl>
2023-02-23 11:35:30 +0100

    login page gemaakt

---
+++ src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import LoginPage from '../views/LoginPage.vue';
+import HomePage from '../views/HomePage.vue';
+import RegisterPage from '../views/RegisterPage.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginPage
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterPage
+  },
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/HomePage.vue
@@ -0,0 +1,45 @@
+<template>
+    <ion-page>
+      <ion-header>
+        <ion-toolbar>
+           <ion-title>Home Page</ion-title>
+        </ion-toolbar>
+      </ion-header>
+  
+      <ion-content>
+        <ion-list>
+            <ion-item>
+              <ion-button router-link="/new-competition" router-direction="forward" >New Competition</ion-button>
+            </ion-item>
+            <ion-item>
+              <ion-button router-link="/join-tournament" router-direction="forward" >Join Tournament</ion-button>
+            </ion-item>
+            <ion-item>
+              <ion-button router-link="/scan" router-direction="forward" >Scan hand</ion-button>
+            </ion-item>
+        </ion-list>
+
+      </ion-content>
+    </ion-page>
+  </template>
+    
+    <script>
+    import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+    import { defineComponent, ref } from 'vue';
+    import { useRouter } from 'vue-router';
+  
+    export default defineComponent({
+      name: 'HomePage',
+      components: {
+        IonContent,
+        IonHeader,
+        IonPage,
+        IonTitle,
+        IonToolbar
+        },
+      setup() {
+  
+          return {router: useRouter()};
+      },
+    });
+  </script>
