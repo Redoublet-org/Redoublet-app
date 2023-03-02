cc01026884f1ea1116ed7ac369ea1cd07ef458f9
Koekiemonster69 <74185498+Koekiemonster69@users.noreply.github.com>
2023-03-02 13:07:24 +0100

    halfway
    even comitten want ik was met het verkeerde account ingelogd

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,9 +3,22 @@ import { RouteRecordRaw } from 'vue-router';
 import LoginPage from '../views/LoginPage.vue';
 import HomePage from '../views/HomePage.vue';
 import RegisterPage from '../views/RegisterPage.vue';
-import NewCompPage from '../views/NewCompPage.vue'
-import JoinTourPage from '../views/JoinTourPage.vue'
-import ScanPage from '../views/ScanPage.vue'
+import NewCompPage from '../views/NewCompPage.vue';
+import JoinTourPage from '../views/JoinTourPage.vue';
+import ScanPage from '../views/ScanPage.vue';
+
+import SearchTourPage from '../views/JoinTournament/SearchTourPage.vue';
+import OpenToursPage from '../views/JoinTournament/OpenToursPage.vue';
+import WaitForTourStartPage from '../views/JoinTournament/WaitForTourStartPage.vue';
+import WalkingSchemePage from '../views/JoinTournament/WalkingSchemePage.vue';
+import InputScorePage from '../views/JoinTournament/InputScorePage.vue';
+/*import ScanHandsSouthPage from '../views/JoinTournament/ScanHandsSouthPage.vue';
+import ScanHandsWestPage from '../views/JoinTournament/ScanHandsWestPage.vue';
+import ScanHandsNorthPage from '../views/JoinTournament/ScanHandsNorthPage.vue';
+import ScanHandsEastPage from '../views/JoinTournament/ScanHandsEastPage.vue';
+import ScoringDDSPage from '../views/JoinTournament/ScoringDDSPage.vue';
+import WaitForTourEndPage from '../views/JoinTournament/WaitForTourEndPage.vue';
+import TourEndedPage from '../views/JoinTournament/TourEndedPage.vue'; */
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -41,7 +54,67 @@ const routes: Array<RouteRecordRaw> = [
     path: '/scan',
     name: 'Scan',
     component: ScanPage
-  }
+  },
+  {
+    path: '/join-tournament/search',
+    name: 'SearchTournament',
+    component: SearchTourPage
+  },
+  {
+    path: '/join-tournament/join',
+    name: 'OpenTournaments',
+    component: OpenToursPage
+  },
+  {
+    path: '/join-tournament/wait-start',
+    name: 'WaitForTourStart',
+    component: WaitForTourStartPage
+  },
+  {
+    path: '/join-tournament/walking-scheme',
+    name: 'WalkingScheme',
+    component: WalkingSchemePage
+  },
+  {
+    path: '/join-tournament/input-score',
+    name: 'InputScore',
+    component: InputScorePage
+  },
+  /*{
+    path: '/join-tournament/scan-hand-south',
+    name: 'ScanHandsTour',
+    component: ScanHandsSouthPage
+  },
+  {
+    path: '/join-tournament/scan-hand-west',
+    name: 'ScanHandsTour',
+    component: ScanHandsWestPage
+  },
+  {
+    path: '/join-tournament/scan-hand-north',
+    name: 'ScanHandsTour',
+    component: ScanHandsNorthPage
+  },
+  {
+    path: '/join-tournament/scan-hand-east',
+    name: 'ScanHandsTour',
+    component: ScanHandsEastPage
+  },
+  {
+    path: '/join-tournament/scoring-and-DDS',
+    name: 'ScoringDDS',
+    component: ScoringDDSPage
+  },
+  {
+    path: '/join-tournament/waiting-for-end',
+    name: 'WaitForTourEnded',
+    component: WaitForTourEndPage
+  },
+  {
+    path: '/join-tournament/end',
+    name: 'TournamentEnded',
+    component: TourEndedPage
+  }*/
 
 ]
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -12,7 +12,7 @@
               <ion-button router-link="/new-competition" router-direction="forward" >New Competition</ion-button>
             </ion-item>
             <ion-item>
-              <ion-button router-link="/join-tournament" router-direction="forward" >Join Tournament</ion-button>
+              <ion-button router-link="/join-tournament/search" router-direction="forward" >Join Tournament</ion-button>
             </ion-item>
             <ion-item>
               <ion-button router-link="/scan" router-direction="forward" >Scan hand</ion-button>
